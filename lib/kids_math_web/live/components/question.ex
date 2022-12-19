44083b9d5313ce045bbe6822e21e0bfdbf9a4093
marc@roosoft.com <marc@roosoft.com>
2022-12-19 10:02:06 -0500

    working example

--- lib/kids_math_web/live/components/question.ex
+++ lib/kids_math_web/live/components/question.ex
@@ -4,7 +4,16 @@ defmodule KidsMathWeb.Components.Question do
   def question(assigns) do
     ~H"""
     <span class="text-9xl">
-      <%= @addends.left %> + <%= @addends.right %> = <%= elem(@attempt, 1) %>
+      <%= @addends.left %> + <%= @addends.right %> =
+      <span class={
+        case elem(@attempt, 0) do
+          :right -> "text-green-500"
+          :wrong -> "text-red-500"
+          _ -> "text-white"
+        end
+      }>
+        <%= elem(@attempt, 1) %>
+      </span>
     </span>
     """
   end
